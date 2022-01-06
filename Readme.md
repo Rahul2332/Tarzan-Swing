ec2e72a337f2c6788a72ecc8d85352a0be9895aa
Rahul2332 <79003706+Rahul2332@users.noreply.github.com>
2022-01-06 23:23:58 +0530

    Add files via upload
    CPP Project by Group27

---
+++ Readme.md
@@ -0,0 +1,53 @@
+# Tarzan Swing by Group27
+
+** Group Members **
+IMT2020117
+IMT2020558
+IMT2020105
+IMT2020021
+IMT2020067
+
+** GamePlay **
+https://iiitbac-my.sharepoint.com/:f:/g/personal/rahul_jain_iiitb_ac_in/El5t2SQ8Y09LhfGCPyrUin8Bb24NU_nWgFrmBbHlTrjidg?e=56dO3n
+
+** Github Repo **
+
+
+
+** Contribution: **
+
+IMT2020021 Chakradhar Varanasi
+- Created Main Menu
+- Created End Menu
+- Score Calculation
+
+IMT2020105 Rakshit Bang
+- Vector Algebra
+- Lava Creation
+- Collision Detection
+
+IMT2020117 Rahul Jain
+- Main, Friend and Enemy Object Creation
+- Rope Creation
+- Circular Motion
+
+IMT2020558 Ram
+- Background Music
+- Collision Sounds
+- Producing linear gradiant colors and other graphical enhancement  
+
+IMT2020067 Rishi 
+- Randomized Friend and Enemy Object Spawn
+- Varied Density of objects at different heights
+- makefile
+
+
+
+** Instructions to run the game using terminal: **
+
+- make
+- ./TarzanSwing
+
+
+
+
---
+++ RenderWindow.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+
+#include <vector>
+#include "Object.hpp"
+#include "Me.hpp"
+#include "LTexture.hpp"
+#include "Lava.hpp"
+
+class RenderWindow
+{
+public:
+    RenderWindow(const char* p_title, int p_w, int p_h);
+    SDL_Texture* loadTexture(const char* p_filePath);
+    void cleanUp();
+    void clear();
+    void render(SDL_Texture* p_tex);
+    void display();
+    void drawObject(Me* obj);
+    void drawEnemyObject(std::vector<Object*> enemyList);
+    void drawFrendObject(std::vector<Object*> frendList);
+    void drawLine(Me* o1, Object* o2);
+    void main_menu(LTexture* TextTexture1, LTexture* TextTexture2, LTexture* TextTexture3);
+    void score_menu(int TotalScore, LTexture* TextTexture1, LTexture* TextTexture2);
+    void end_menu(int TotalScore,  int HighScore, LTexture* TextTexture, LTexture* TextTexture1, LTexture* TextTexture2, LTexture* TextTexture3, LTexture* TextTexture4, LTexture* TextTexture5, LTexture* TextTexture6);
+    void drawLava(Lava* lava);
+    SDL_Window* get_renderer_window();
+    SDL_Renderer* get_SDL_renderer();
+private:
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+};
